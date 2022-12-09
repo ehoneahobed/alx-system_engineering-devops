f6bca46721d68f12db5680e1ac93b7176c27c2c1
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-12-09 17:08:52 +0000

    Add solution to task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This manifest creats a file in /tmp/
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love puppet',
+}
