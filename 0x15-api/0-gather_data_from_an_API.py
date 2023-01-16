5c53da3732615d94b52545241c9dbb272edc127a
ehoneahobed <ehoneahobed@hotmail.com>
2023-01-16 16:56:16 +0000

    Update solution to task 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -21,7 +21,8 @@ if __name__ == '__main__':
             done_tasks.append(task)
             done += 1
 
-    print("Employee {} is done with tasks({}/{}):".format(employeeName, done, len(tasks)))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
 
     for task in done_tasks:
         print("\t {}".format(task.get('title')))
